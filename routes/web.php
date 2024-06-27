9f67c30f0facd6e324a7e95de6347b707b6f7e05
osuntuyi john <osun0001@hz.nl>
2024-06-27 21:15:35 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,42 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 /*
  * The home routes
  */
-Route::get('/', [TaskController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+/*
+ * The protected home route
+ */
+Route::middleware(['auth'])->get('/home', [TaskController::class, 'index'])->name('home');
+
 
 /*
  * The resource routes
  */
-Route::resource('tasks', TaskController::class)
-    ->except(['index']); // removed this route because this is moved to the home route
-Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])
-    ->name('tasks.complete');
-Route::resource('posts', PostController::class);
-Route::resource('projects', ProjectController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('tasks', TaskController::class)->except(['index']);
+    Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+    Route::resource('posts', PostController::class);
+    Route::resource('projects', ProjectController::class);
+});
 
 /*
  * Route that shows the about page. This handler just returns the about view.
  */
 Route::view('/about', 'about')->name('about');
+
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
+require __DIR__.'/auth.php';
