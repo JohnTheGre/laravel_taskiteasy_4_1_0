b3d6c7d14656eace88198f6105f259320a1f1b91
osuntuyi john <osun0001@hz.nl>
2024-06-27 19:47:27 +0200

    message

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,24 +14,18 @@ use Illuminate\Support\Facades\URL;
 |
 */
 
-/*
- * The home routes
- */
-Route::get('/', [TaskController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-/*
- * The resource routes
- */
-Route::resource('tasks', TaskController::class)
-    ->except(['index']); // removed this route because this is moved to the home route
-Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])
-    ->name('tasks.complete');
-Route::resource('posts', PostController::class);
-Route::resource('projects', ProjectController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-/*
- * Route that shows the about page. This handler just returns the about view.
- */
-Route::view('/about', 'about')->name('about');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-URL::forceScheme('https');
+require __DIR__.'/auth.php';
