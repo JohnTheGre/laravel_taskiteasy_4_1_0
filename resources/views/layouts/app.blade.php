5f2e28d7db6cf628c7fdd9d6cbc06530562d38c0
osuntuyi john <osun0001@hz.nl>
2024-06-27 20:41:48 +0200

    message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- Compiled assets --}}
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        {{-- Render additional styles from subviews and/or components --}}
+        @stack('styles')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
