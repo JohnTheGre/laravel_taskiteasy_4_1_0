6d75ef68cfc4618b06277628878f7ff2274ceba3
osuntuyi john <osun0001@hz.nl>
2024-06-28 21:46:35 +0200

    message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,12 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
         {{-- Compiled assets --}}
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         {{-- Render additional styles from subviews and/or components --}}
         @stack('styles')
     </head>
